8d2b7423cc98f8120fbf0b035fffd7df5aeba8b3
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-07 10:52:10 +0100

    Update NumProgEx.hs

--- Baby's First Haskell/NumProgEx/NumProgEx.hs
+++ Baby's First Haskell/NumProgEx/NumProgEx.hs
@@ -3,9 +3,9 @@ readInts s = let ws = words s in map read ws
 
 minMax :: Ord a => [a] -> Maybe (a, a)
 minMax (h : t) = Just $ foldr
-  (\x (min, max) -> (if x < min then x else min, if x > max then x else max))
-  (h, h)
-  t
+  (\x (min, max) -> (if x < min then x else min, if x > max then x else max))  -- \x is similar to lamda func
+  (h, h)  -- head
+  t  -- tail
 minMax _ = Nothing
 
 main :: IO ()
