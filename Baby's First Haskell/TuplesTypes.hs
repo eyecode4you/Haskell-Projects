97689cebe2387e09b9e2b0e0056782ca4bbb3ac0
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-12 16:20:48 +0100

    Create TuplesTypes.hs

---
+++ Baby's First Haskell/TuplesTypes.hs
@@ -0,0 +1,8 @@
+t0 :: (Int, Int)
+t0 = (1234, 5678)
+
+t1 :: (String, Int, Double)  -- heterogeneous elements
+t1 = ("sometext", 8, 3.141)
+
+t2 :: ([Int], [String], (Float, Char))
+t2 = ([1, 2, 3, 4], ["aaa", "bbb"], (1.0, 'o'))
