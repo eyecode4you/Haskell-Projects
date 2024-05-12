d48c437ac8748a9fe81043e825ed657c5ff862be
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-12 16:01:22 +0100

    Create TypeSynonyms.hs

---
+++ Baby's First Haskell/TypeSynonyms.hs
@@ -0,0 +1,3 @@
+type Port = Int  -- :info Port  type Port = Int  - Port is an alias for Int
+type HostInfo = (String, Int)  -- Tuple
+type HostInfo = (String, Port)  -- can also pass other aliases in
