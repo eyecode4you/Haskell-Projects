512e2b1a0fe14933a8fc983da590e696ad2e6c0f
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-17 22:59:40 +0100

    Update 2: SearchingForWord.hs

--- WordSearchGame/Test/2: SearchingForWord.hs
+++ WordSearchGame/Test/2: SearchingForWord.hs
@@ -1,9 +1,5 @@
 -- format and print our wordsearch grid with words placed inside
 import Data.List
-
-findWordInLine :: String -> String -> Bool -- we need to compare two strings, check if one contains the other, it either does or does not
-findWordInLine word line = undefined
-
 main :: do
 -- we can search functions that do String -> String -> Bool on hoogle.haskell.org. E.g. using isInfixOf - https://hackage.haskell.org/package/basement-0.0.16/docs/Basement-String.html#v:isInfixOf
           print (isInfixOf "HASKELL" "LKHVPWIHASKELL OWPJC") -- True
