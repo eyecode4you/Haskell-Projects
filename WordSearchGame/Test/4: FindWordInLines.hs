24a8843aae34da2b73f3d94864034e2b23dc5959
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-17 23:33:15 +0100

    Update 4: FindWordInLines.hs

--- WordSearchGame/Test/4: FindWordInLines.hs
+++ WordSearchGame/Test/4: FindWordInLines.hs
@@ -1,1 +1,14 @@
 
+import Data.List (isInfixOf)
+findWordInLine :: String -> String -> Bool
+findWordInLine word line = word `isInfixOf` line
+
+grid = ["ONE", "TWO", "THREE", "FOUR", "FIVE"]  -- test list
+
+main :: IO ()
+main = do
+        print grid
+        -- check for word in all lines. We can use map:
+        print (map (findWordInLine "THREE") grid) -- returns list of bools for check result in every line: [False,False,True,False,False]
+        -- check ^^^ list if any is True. We can use Logical OR:
+        print (or (map (findWordInLine "THREE") grid)) -- True
