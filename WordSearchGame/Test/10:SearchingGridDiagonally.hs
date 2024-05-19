32deff21142ce7ce3fa6021909780a07a7bdb60d
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-19 06:16:21 +0100

    Update and rename 10:SearchInAllDirectionsOfGrid.hs to 10:SearchingGridDiagonally.hs

---
+++ WordSearchGame/Test/10:SearchingGridDiagonally.hs
@@ -0,0 +1,19 @@
+
+import Data.List (Transpose)
+type Grid = [String]
+
+grid = [ "N___________Q__"
+       , "W__O_______R___"
+       , "O_C_O_____A____"
+       , "D_O__B_U_DN____"
+       , "E_M___ENA_A___O"
+       , "D_P____R__D___Y"
+       , "I_T____EEDOCEYE"
+       , "S_IBM_MA__H___H"
+       , "PHASKELL__S____"
+       , "U_______L______"
+       ]
+
+main :: IO ()
+main = do
+        print ""
