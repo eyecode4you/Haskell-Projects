863faa733c2b7293cce99766b4369c178f7dc0a3
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2024-05-20 06:47:07 +0100

    Create Spec.hs

---
+++ WordSearchGame/Test/Unit Testing/Spec.hs
@@ -0,0 +1,16 @@
+main :: IO ()
+main = hspec $ do
+    describe "formatGrid" $ do
+      it "Should concatenate every line with a newline" $ do
+        (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
+    
+    describe "findWord" $ do
+      it "Should find words that exist in wordsearch grid" $ do
+        findWord grid "REBOOT" `shouldBe` Just "REBOOT"
+        findWord grid "QRADAR" `shouldBe` Just "QRADAR"
+      it "Should not find words that don't exist in grid" $ do
+        findWord grid "DIGERIDOO" `shouldBe` Nothing
+    
+    describe "matchedWords" $ do
+      it "Should find all words present in grid" $ do
+        matchedWords grid searchws `shouldBe` searchws
